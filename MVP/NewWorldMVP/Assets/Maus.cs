02c830575223c97cb4af6340b575ff7ad4f97271
Ymmer <30795204+YvesImmer@users.noreply.github.com>
2017-09-18 15:08:43 +0200

    Hex Color switching
    When clicking on a hex it changes color! This is achieved with the "maus" script of the hex prefab.

--- MVP/NewWorldMVP/Assets/Maus.cs
+++ MVP/NewWorldMVP/Assets/Maus.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Maus : MonoBehaviour {
+	
+	Color terrainColor = Color.blue;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
+		
 	void OnMouseDown()
 	{
-		Debug.Log("Mouse Clicked");
+		if (terrainColor == Color.blue) {
+			terrainColor = Color.red; 
+		}
+		else if(terrainColor == Color.red) {
+			terrainColor = Color.blue;
+		}
+
+		GetComponent<SpriteRenderer>().color = terrainColor;
+
 	}
 	// Update is called once per frame
 	void Update () {
