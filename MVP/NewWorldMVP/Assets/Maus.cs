57296a757ca47a954ebd71564c12ff051b5f8074
Ymmer <30795204+YvesImmer@users.noreply.github.com>
2017-09-25 14:10:13 +0200

    Mouse script 2.0
    Now the color of hexes is determined by the variable "state" when clicked on the script checks what state the hex is in via switch statement and does a thing, currently switching states and changing the color accordingly.

--- MVP/NewWorldMVP/Assets/Maus.cs
+++ MVP/NewWorldMVP/Assets/Maus.cs
@@ -3,24 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Maus : MonoBehaviour {
-	
-	Color terrainColor = Color.blue;
+	//state = 0 undiscovered
+	//state = 1 discovered
+	//state = 2 explored
+	//state = 3 conquered
+	public int state = 0;
+
+	//the number of states, used for changing states.
+	int numberOfStates = 4;
+
+	//The colors corespond to their states, color0 for state 0 color 1 for state 1 etc.
+	Color color0 = Color.black;
+	Color color1 = Color.blue;
+	Color color2 = Color.green;
+	Color color3 = Color.yellow;
 
 	// Use this for initialization
-	void Start () {
 
+	void ChangeColor (Color newColor){
+		GetComponent<SpriteRenderer>().color = newColor;
+		Debug.Log ("Changing Color to " + newColor);
 	}
-		
-	void OnMouseDown()
-	{
-		if (terrainColor == Color.blue) {
-			terrainColor = Color.red; 
-		}
-		else if(terrainColor == Color.red) {
-			terrainColor = Color.blue;
-		}
 
-		GetComponent<SpriteRenderer>().color = terrainColor;
+	void Start () {
+		ChangeColor (color0);
+	}
+
+	void OnMouseDown(){
+		//
+		ChangeState ((state + 1)%numberOfStates);
+		}
+		
+	void ChangeState(int newState){
+		state = newState;
+		Debug.Log ("Changed state to " + newState);
+
+		switch (state) {
+		case 0:
+			ChangeColor (color0);
+			break;
+
+		case 1:
+			ChangeColor (color1);
+			break;
+
+		case 2:
+			ChangeColor (color2);
+			break;
+		
+		case 3:
+			ChangeColor (color3);
+			break;
+
+		default:
+			Debug.Log("Error: Unexpected state");
+			break;
+		}
 
 	}
 	// Update is called once per frame
