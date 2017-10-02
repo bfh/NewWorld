af309c63e46341080a003950300a66351f93f2e1
yanndara <31209353+yanndara@users.noreply.github.com>
2017-10-02 15:17:13 +0200

    camera zoom in and out
    now using ortogonal system so it stays 2d

--- MVP/NewWorldMVP/Assets/Scripts/C#/cameraMovement.cs
+++ MVP/NewWorldMVP/Assets/Scripts/C#/cameraMovement.cs
@@ -4,50 +4,61 @@ using UnityEngine;
 
 public class cameraMovement : MonoBehaviour
 {
+    public Camera cam;
+
 
     // Use this for initialization
     void Start()
-    {
-
+    { GameObject controller = GameObject.FindGameObjectWithTag("GameController");
+         cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject controller = GameObject.FindGameObjectWithTag("GameController");
+        double camX = controller.GetComponent<HexGrid>().x * 0.88;
+        double camY = controller.GetComponent<HexGrid>().y * 0.76210233333;
+
         int speed = 2;
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if (transform.position.x < 25)
-            { transform.Translate(new Vector3(speed * (-transform.position.z / 10) * Time.deltaTime, 0, 0)); }
+            if (transform.position.x < camX)
+            {
+            
+                transform.Translate(new Vector3(speed * (cam.orthographicSize / 2) * Time.deltaTime, 0, 0)); }
 
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             if (transform.position.x > 0)
-            { transform.Translate(new Vector3(-speed * (-transform.position.z / 10) * Time.deltaTime, 0, 0)); }
+            { transform.Translate(new Vector3(-speed * (cam.orthographicSize / 2) * Time.deltaTime, 0, 0)); }
 
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             if (transform.position.y > 0)
-            { transform.Translate(new Vector3(0, -speed * (-transform.position.z / 10) * Time.deltaTime, 0)); }
+            { transform.Translate(new Vector3(0, -speed * (cam.orthographicSize / 2) * Time.deltaTime, 0)); }
 
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            if (transform.position.y < 10)
-            { transform.Translate(new Vector3(0, speed * (-transform.position.z / 10) * Time.deltaTime, 0)); }
+            if (transform.position.y < camY)
+            { transform.Translate(new Vector3(0, speed * (cam.orthographicSize / 2) * Time.deltaTime, 0)); }
         }
         if (Input.GetKey(KeyCode.Q))
         {
-            if (transform.position.z < -2)
-            { transform.Translate(new Vector3(0, 0, 2 * Time.deltaTime)); }
-
+            if (cam.orthographicSize > 1)
+            {
+                cam.orthographicSize -= 0.1f;
+            }
         }
         if (Input.GetKey(KeyCode.W))
         {
-            if (transform.position.z > -25)
-            { transform.Translate(new Vector3(0, 0, -2 * Time.deltaTime)); }
+            if (cam.orthographicSize < 18 )
+            {
+                cam.orthographicSize += 0.1f;
+            }
 
         }
     }
