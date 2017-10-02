f5286f53e75346b8fc69f61a33073e62cc270a1f
yanndara <31209353+yanndara@users.noreply.github.com>
2017-10-02 13:31:48 +0200

    Camera Zoom in and out
    added zoom in and out with speed change

--- MVP/NewWorldMVP/Assets/Scripts/C#/cameraMovement.cs
+++ MVP/NewWorldMVP/Assets/Scripts/C#/cameraMovement.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraMovement : MonoBehaviour {
+public class cameraMovement : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,19 +17,38 @@ public class cameraMovement : MonoBehaviour {
         int speed = 2;
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            if (transform.position.x < 25)
+            { transform.Translate(new Vector3(speed * (-transform.position.z / 10) * Time.deltaTime, 0, 0)); }
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            if (transform.position.x > 0)
+            { transform.Translate(new Vector3(-speed * (-transform.position.z / 10) * Time.deltaTime, 0, 0)); }
+
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            if (transform.position.y > 0)
+            { transform.Translate(new Vector3(0, -speed * (-transform.position.z / 10) * Time.deltaTime, 0)); }
+
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            if (transform.position.y < 10)
+            { transform.Translate(new Vector3(0, speed * (-transform.position.z / 10) * Time.deltaTime, 0)); }
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (transform.position.z < -2)
+            { transform.Translate(new Vector3(0, 0, 2 * Time.deltaTime)); }
+
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (transform.position.z > -25)
+            { transform.Translate(new Vector3(0, 0, -2 * Time.deltaTime)); }
+
         }
     }
 }
